a251ec4938b7f9a3125d5cd8e89059c751173695
sean <sobringer@collineargroup.com>
2019-02-08 15:38:04 -0800

    added leger to graph

--- src/main/Content.jsx
+++ src/main/Content.jsx
@@ -8,15 +8,39 @@ import { fakeData } from '../fake-data'
 export default class Content extends Component {
   // @observable
   plotState = {
-    data: [{
-      x: fakeData.initialDataSet.map(datum => new Date(datum[0])),
-      y: fakeData.initialDataSet.map(datum => datum[1]),
+    data:[{
+      x: [0, 1, 2, 3, 4, 5, 6, 7, 8],
+      y: [0, 3, 6, 4, 5, 2, 3, 5, 4],
       type: 'scatter',
-      mode: 'lines',
+      mode:'lines',
+      name:'test1',
       marker: { color: 'purple' }
+    },
+    {
+      x: [0, 1, 2, 3, 4, 5, 6, 7, 8],
+      y: [0, 4, 7, 8, 3, 6, 3, 3, 4],
+      type: 'scatter',
+      mode:'lines',
+      name:'test2'
     }],
+    
+    // [{
+    //   x: fakeData.initialDataSet.map(datum => new Date(datum[0])),
+    //   y: fakeData.initialDataSet.map(datum => datum[1]),
+    //   type: 'scatter',
+    //   mode: 'lines',
+    //   marker: { color: 'purple' }
+    // }, {
+    //   x: fakeData.initialDataSet.map(datum => new Date(datum[0])),
+    //   y: fakeData.initialDataSet.map(datum => datum[1]),
+    //   type: 'scatter',
+    //   mode: 'lines',
+    //   marker: { color: 'yellow' }
+    // }],
     layout: {
       autosize: true,
+      showlegend:true,
+      legend:{ x:0, y:4, orientation:'h'},
       xaxis: {
         // rangeselector: {buttons: [
         //     {
