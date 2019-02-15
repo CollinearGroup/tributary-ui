0ef0c4e546e93fa3589cbf5155d621a9224666e1
George Barta <george.barta@gmail.com>
2019-02-15 13:39:00 -0800

    Display clean default message for analysis tab

--- src/sidebar/Analysis.jsx
+++ src/sidebar/Analysis.jsx
@@ -8,22 +8,22 @@ class Analysis extends Component {
     render(){
       return (
         <div className='analysis-container'>
-          <div className="card">
-            <div className='analysis-card-title'>
-              {this.props.activeDataSeriesStore.hoverData.length &&
-                //show the date
-                this.props.activeDataSeriesStore.hoverData[0].x
-              }
-            </div>
-            {this.props.activeDataSeriesStore.hoverData.map(pt => {
-              return <div key={pt.name+'_'+pt.x} className="analysis-card-series">
-                <div className='analysis-card-series-name'>{pt.name}</div>
-                <div className='analysis-card-value'>
-                  {pt.y}
-                </div>
+          {(this.props.activeDataSeriesStore.hoverData.length &&
+            <div className="card">
+              <div className='analysis-card-title'>
+                {this.props.activeDataSeriesStore.hoverData[0].x}
               </div>
-            })}
-          </div>
+              {this.props.activeDataSeriesStore.hoverData.map(pt => {
+                return <div key={pt.name + '_' + pt.x} className="analysis-card-series">
+                  <div className='analysis-card-series-name'>{pt.name}</div>
+                  <div className='analysis-card-value'>
+                    {pt.y}
+                  </div>
+                </div>
+              })}
+            </div>
+          ) || <p className='analysis-default-message'>No data selected</p>
+          }
         </div>
       )
     }
