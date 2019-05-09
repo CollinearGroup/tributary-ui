6a320ea7b50e8f4388bec0d8e9296143d6900f7c
cpeterson <cpeterson@collineargroup.com>
2019-05-09 13:57:38 -0700

    adds safety checks for data structure of incoming attribute options

--- src/sidebar/DataSourcePropertyInput.jsx
+++ src/sidebar/DataSourcePropertyInput.jsx
@@ -15,10 +15,16 @@ const DataSourcePropertyInput = ({property, onChange, currentValue, serviceName}
 
     let reorder = []
     let options = []
-
     switch (type) {
       case 'object' :
-        //initial structure should be {key:val}
+        //initial structure should be {'string':'string'}
+        //safety checks for structure
+        if(!Object.keys(values).length 
+        || typeof Object.keys(values)[0] !== 'string'
+        || typeof values[Object.keys(values)[0]] !== 'string') {
+          return []
+        }
+
         //ordering array of keys by its values
         reorder = Object.keys(values).sort((a,b) => {
           let A = values[a]
@@ -36,19 +42,34 @@ const DataSourcePropertyInput = ({property, onChange, currentValue, serviceName}
         return options
       case 'array' :
         //initial structure should be [{name:'string',value:'string',order:int}]
+        //safety checks for structure
+        if(!values.length 
+          || typeof values[0] !== 'object'
+          || Array.isArray(values[0])){
+            return []
+          }
+
         //orders array of obj by obj.order
-        reorder = values.sort((a,b) => a.sort-b.sort)
+        reorder = values.sort((a,b) => {
+          //sends object to end if no 'order' key. Additional check bc 0 is falsy
+          let A = (a.order || a.order === 0) ? a.order : Infinity
+          let B = (b.order || b.order === 0) ? b.order : Infinity
+          return A-B
+        })
         //maps each obj into options
         options = reorder.map(obj => {
+          //sets defaults if no obj.name or obj.value is present
+          let key = obj.name ? obj.name : 'key'
+          let value = obj.value ? obj.value : 'value'
           return (
-            <option key={obj.name} value={obj.name}>
-             {obj.value}
+            <option key={key} value={key}>
+             {value}
             </option>
           )
         })
         return options
       default:
-        return values
+        return []
     }
   }
 
